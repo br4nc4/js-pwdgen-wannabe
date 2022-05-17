8fe28735b7de86a3647e3c2a72be24b66d032af2
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-17 12:43:58 +0200

    test JS completed

--- js/script.js
+++ js/script.js
@@ -6,3 +6,6 @@ surnameUser.innerHTML = prompt("Qual'è il tuo cognome?")
 
 const colorFavUser = document.getElementById("third-input");
 colorFavUser.innerHTML = prompt("Qual'è il tuo colore preferito?")
+
+const numberOutput = document.getElementById("num-output");
+numberOutput.innerHTML = "22"
