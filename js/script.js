b78aa92080ef580e031ade69cf6c13c65c01dda7
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-17 12:31:45 +0200

    testing JS pt.2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,2 @@
-const nameUser = prompt("Qual'è il tuo nome?")
-const surnameUser = prompt("Qual'è il tuo cognome?")
-const colorFav = prompt("Qual'è il tuo colore preferito?")
-
+const userName = document.getElementById("first-input");
+userName.innerHTML = prompt("Qual'è il tuo nome?")
