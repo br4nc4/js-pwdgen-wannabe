7fad384af74fe7ba16aef982c23cae68759b044f
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-17 12:38:36 +0200

    testing JS pt.3

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-const userName = document.getElementById("first-input");
-userName.innerHTML = prompt("Qual'è il tuo nome?")
+const nameUser = document.getElementById("first-input");
+nameUser.innerHTML = prompt("Qual'è il tuo nome?")
+
+const surnameUser = document.getElementById("second-input");
+surnameUser.innerHTML = prompt("Qual'è il tuo cognome?")
+
+const colorFavUser = document.getElementById("third-input");
+colorFavUser.innerHTML = prompt("Qual'è il tuo colore preferito?")
